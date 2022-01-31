6d4d5a0931d10006ab2626a8582a4a2dda14493e
navenduproject <98395321+navenduproject@users.noreply.github.com>
2022-01-31 17:09:30 +0000

    Add files via upload

---
+++ EC2_Check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PrivateIP=`aws ec2 describe-instances --filters Name=tag:Name,Values=Project1 --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text`
+
+if [ -z "$PrivateIP" ]
+then
+        echo "Creating Project1 EC2 instance"
+        ansible-playbook playbook1.yml
+        ansible-playbook playbook2.yml
+else
+        echo "Project1 EC2 instance already found, hence running docker built on $PrivateIP"
+        ansible-playbook playbook3.yml
+fi
